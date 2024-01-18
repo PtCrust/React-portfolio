2e78e1561a5fc97cb1ac56f730c77049a39c44cf
Tony <61390876+PtCrust@users.noreply.github.com>
2024-01-18 20:43:16 +0200

    Add files via upload

---
+++ src/components/5-footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="flex">
+      <ul className="flex">
+        <li><a>About</a></li>
+        <li><a>Home</a></li>
+        <li><a>Tony</a></li>
+        <li><a>Sameh</a></li>
+      </ul>
+      <p>This page made by <code>./Eng/Tony/Sameh</code></p>
+    </footer>
+  )
+}
---
+++ src/components/db.js
@@ -0,0 +1,121 @@
+
+const projectCard = [
+
+    {
+        id: 1,
+        category: ["certificate"],
+        title: "CCNA course",
+        desc: "Scal academy",
+        imgS: "./images/ccna.jpg",
+        imgA: "ccna",
+        github: "",
+        link: "./images/ccna.jpg"
+    },
+    {
+        id: 2,
+        category: ["certificate"],
+        title: "Backend course php & Laravel",
+        desc: "php , laravel , phpmyadmin , mysql , bootstrap",
+        imgS: "./images/cls.jpg",
+        imgA: "backend img",
+        github: "",
+        link: "./images/cls.jpg"
+    },
+    {
+        id: 3,
+        category: ["certificate"],
+        title: "FrontEnd course Reactjs",
+        desc: "Reactjs",
+        imgS: "./images/frontEnd.jpg",
+        imgA: "CV",
+        github: "",
+        link: "./images/frontEnd.jpg"
+        ,
+    },
+    {
+        id: 4,
+        category: ["certificate"],
+        title: "Google IT Support Specialist course 2",
+        desc: "final grade: 98%",
+        imgS: "./images/bit.png",
+        imgA: "Google course 2",
+        github: "",
+        link: "./images/bit.png"
+    },
+    {
+        id: 5,
+        category: ["project"],
+        title: "Furniture Website",
+        desc: "desc",
+        imgS: "./images/furniture1.png",
+        imgA: "furniture img",
+        github: "https://github.com/PtCrust/Furniture-Website",
+        link: "https://ptcrust.github.io/Furniture-Website/"
+    },
+    {
+        id: 6,
+        category: ["project"],
+        title: "Graduation project",
+        desc: "Final grade: Excellent",
+        imgS: "./images/Gproject.png",
+        imgA: "Graduation project img",
+        github: "",
+        link: "./images/Gproject.png"
+    },
+    {
+        id: 7,
+        category: ["certificate"],
+        title: "Google IT Support Specialist course 1",
+        desc: "Final grade: 98%",
+        imgS: "./images/it.png",
+        imgA: "Google course 1",
+        github: "",
+        link: "./images/it.png"
+
+    },
+    {
+        id: 8,
+        category: ["project", "react"],
+        title: "React project",
+        desc: "E-commerce website",
+        imgS: "./images/reactApp.png",
+        imgA: "reactApp",
+        github: "https://github.com/PtCrust/React-Ecommerce-App",
+        link: "https://tony-reactapp.netlify.app/"
+
+    },
+    {
+        id: 9,
+        category: ["project", "react"],
+        title: "React project",
+        desc: "CV website",
+        imgS: "./images/react-portfolio.png",
+        imgA: "cv",
+        github: "https://github.com/PtCrust/React-portfolio",
+        link: ""
+
+    },
+    {
+        id: 10,
+        category: ["project"],
+        title: "Old CV website",
+        desc: "Html , css , javascript",
+        imgS: "./images/oldCvWebsite.png",
+        imgA: "Old-cv",
+        github: "https://github.com/PtCrust/My-CV.com",
+        link: "https://ptcrust.github.io/My-CV.com/"
+
+    },
+    {
+        id: 11,
+        category: ["CV"],
+        title: "cv.pdf",
+        desc: "Resume",
+        imgS: "./images/tony sameh.pdf",
+        imgA: "cv.pdf",
+        github: "",
+        link: "./images/tony sameh.pdf"
+
+    }
+]
+export default projectCard;
